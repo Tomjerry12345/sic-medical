05bfd35f2439152e183da25d6398c1c84021dc6f
tomjerry <Andrialfiandi46@gmail.com>
2023-05-06 18:02:24 +0800

    06/05/2023

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getAsync } from "../redux/main/main.thunks";
 import {
   getLocal, log,
 } from "../values/Utilitas";
