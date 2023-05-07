91a1b753449b717a1f1aecef79083d7dc968a0e6
amrul07 <amrulahsanullah.aa@gmail.com>
2023-05-07 19:32:16 +0800

    7/5/2023

--- src/pages/autentikasi/login/LoginPage.jsx
+++ src/pages/autentikasi/login/LoginPage.jsx
@@ -1,7 +1,114 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Button, Stack, TextField } from "@mui/material";
+
 const LoginPage = () => {
-    return ( 
-        <h1>Login Page</h1>
-     );
-}
- 
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        height: "100vh",
+        marginTop: 0,
+      }}
+    >
+      {/* Images */}
+      <Grid
+        item
+        sx={{
+          background: "#8BD7EF",
+        }}
+        xs={7}
+      >
+        <Stack
+          sx={{
+            height: "95vh",
+          }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <img alt="" src="images/gambar.png" width="640" height="640" />
+        </Stack>
+      </Grid>
+
+      {/* Typography */}
+      <Stack
+        style={{
+          marginLeft: "35px",
+        }}
+      >
+        <Typography
+          fontSize={36}
+          style={{ fontFamily: "lato", fontWeight: "900" }}
+        >
+          <p>Selamat datang</p>
+        </Typography>
+        <Typography
+          fontSize={29}
+          style={{ width: "80%", fontFamily: "lato", marginTop: "-50px" }}
+        >
+          <p>Silahkan Login Terlebih Dahulu </p>
+        </Typography>
+
+        {/* TextField */}
+        <Stack
+          spacing={3}
+          style={{
+            width: "132%",
+          }}
+        >
+          <TextField label="Email" variant="outlined" />
+          <TextField label="Password" type="password" variant="outlined" />
+        </Stack>
+
+        {/* Login */}
+        <Stack
+          mt={7}
+          style={{
+            marginLeft: "100px",
+            marginRight: "-40px",
+          }}
+        >
+          <Button
+            style={{
+              background: "#8BD7EF",
+              fontFamily: "lato",
+              height: "50px",
+              fontSize: "32px",
+              color: "#FFFFFF",
+              fontWeight: "400",
+            }}
+            direction={"row"}
+            flexWrap="wrap"
+          >
+            Login
+          </Button>
+        </Stack>
+
+        {/* blm punya akun */}
+        <Stack mt={5} style={{ alignItems: "center" }}>
+          <p
+            style={{
+              fontFamily: "lato",
+              fontSize: "30px",
+              marginRight: "-135px",
+            }}
+          >
+            Belum punya akun ?{" "}
+            <a
+              href="#"
+              style={{
+                color: "#8BD7EF",
+                fontWeight: "900",
+              }}
+            >
+              Daftar
+            </a>
+          </p>
+        </Stack>
+      </Stack>
+    </Grid>
+  );
+};
+
 export default LoginPage;
--- src/pages/autentikasi/register/RegisterPage.jsx
+++ src/pages/autentikasi/register/RegisterPage.jsx
@@ -1,7 +1,153 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Button, Stack, TextField, MenuItem } from "@mui/material";
+import AddAPhotoOutlinedIcon from "@mui/icons-material/AddAPhotoOutlined";
+
 const RegisterPage = () => {
-    return (
-        <h1>Register page</h1>
-      );
-}
- 
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        height: "100vh",
+        marginTop: 0,
+      }}
+    >
+      {/* Images */}
+      <Grid
+        item
+        sx={{
+          background: "#8BD7EF",
+        }}
+        xs={7}
+      >
+        <Stack
+          sx={{
+            height: "95vh",
+          }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <img alt="" src="images/gambar.png" width="600" height="600" />
+        </Stack>
+      </Grid>
+
+      {/* Typography */}
+      <Grid
+        style={{
+          marginLeft: "35px",
+          marginTop: "-10px",
+        }}
+        xs={4.5}
+      >
+        <Typography
+          fontSize={36}
+          style={{ fontFamily: "lato", fontWeight: "900" }}
+        >
+          <p>Selamat datang</p>
+        </Typography>
+        <Typography
+          fontSize={26}
+          style={{ width: "60%", fontFamily: "lato", marginTop: "-30px" }}
+        >
+          <p>Silahkan lengkapi form di bawah ini </p>
+        </Typography>
+
+        {/* images */}
+        <Stack
+          mb={2}
+          style={{
+            alignItems: "center",
+            borderRadius: "30%",
+            marginTop: "-40px",
+          }}
+        >
+          <img
+            src="images/luffy.png"
+            width="110px"
+            style={{
+              position: "relative",
+              marginBottom: "14px",
+              zIndex: 0,
+            }}
+          />
+          <AddAPhotoOutlinedIcon
+            style={{
+              background: "#8BD7EF",
+              position: "absolute",
+              marginTop: "70px",
+              marginLeft: "77px",
+              fontSize: "20px",
+              padding: "10px",
+              borderRadius: "5px",
+              color: "white",
+            }}
+          />
+        </Stack>
+
+        {/* TextField */}
+        <Stack spacing={2} style={{}}>
+          <TextField label="Email" variant="outlined" size="small" fullWidth />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            size="small"
+          />
+          <TextField label="Nama lengkap" variant="outlined" size="small" />
+          <TextField label="Gender" select fullWidth size="small">
+            <MenuItem value="laki-laki">Laki-laki</MenuItem>
+            <MenuItem value="perempuan">Perempuan</MenuItem>
+          </TextField>
+        </Stack>
+
+        {/* Button */}
+        <Stack
+          mt={4}
+          style={{
+            width: "100%",
+          }}
+        >
+          <Button
+            style={{
+              marginLeft: "130px",
+              background: "#8BD7EF",
+              fontFamily: "lato",
+              width: "50%",
+              height: "50px",
+              fontSize: "20px",
+              color: "#FFFFFF",
+            }}
+          >
+            Register
+          </Button>
+        </Stack>
+
+        {/* sudah punya akun */}
+        <Stack
+          style={{ alignItems: "center", fontSize: "25px", marginTop: "-10px" }}
+        >
+          <p
+            style={{
+              fontFamily: "lato",
+            }}
+          >
+            Sudah punya akun ?{" "}
+            <a
+              href="#"
+              style={{
+                color: "#8BD7EF",
+                fontWeight: "900",
+                textDecoration: "none",
+              }}
+            >
+              Login
+            </a>
+          </p>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+};
+
 export default RegisterPage;
